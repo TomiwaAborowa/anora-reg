52758e2216b7e58c59f47e1bb583b10c04905908
Tomiwa Aborowa <tomiwaw2013@gmail.com>
2024-11-20 02:12:53 +0100

    brought form back

--- src/components/regForm.js
+++ src/components/regForm.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Clipboard } from "flowbite-react";
+// import { Clipboard } from "flowbite-react";
 import { docFile, txtFile } from "./regAction.config";
 import { v4 } from "uuid";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { addDoc, collection } from "firebase/firestore";
 function RegistrationForm() {
   const [selectedFile, setSelectedFile] = useState(null);
-  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [, setIsButtonDisabled] = useState(true);
 
   // const handleFileChange = (event) => {
   //   setSelectedFile(event.target.files[0]);
@@ -24,7 +24,11 @@ function RegistrationForm() {
   const [text, setText] = useState("");
   const [mail, setMail] = useState("");
   const [dial, setDail] = useState("");
+  const [loc, setLoc] = useState("");
+  const [age, setAge] = useState("");
+  const [gen, setGen] = useState("");
   const [docp, setDocp] = useState("");
+  // eslint-disable-next-line no-unused-vars
   const handleUpload = (e) => {
     setSelectedFile(e.target.files[0]);
     setIsButtonDisabled(false);
@@ -44,6 +48,9 @@ function RegistrationForm() {
       txtVal: text,
       dialVal: dial,
       mailVal: mail,
+      locVal: loc,
+      ageVal: age,
+      genVal: gen,
       pdfVal: docp,
     });
     alert("Data received successfully.");
@@ -52,7 +59,7 @@ function RegistrationForm() {
     <div className="flex flex-col items-center justify-center h-screen">
         <div className="p-12 shadow-2xl bg-gradient-to-tr from-slate-900 to-amber-600 rounded-md">
       <form className="w-full max-w-md">
-      <p className="text-white font-medium">This is the account details:</p>
+      {/* <p className="text-white font-medium">This is the account details:</p>
       <br />
       <p className="text-white text-2xl font-medium">GTB - ANORA MEDIA</p>
       <div className="mt-2 grid w-full max-w-[23rem] grid-cols-8 gap-2 sm:grid-cols-1">
@@ -69,7 +76,7 @@ function RegistrationForm() {
           valueToCopy="0656890371"
           label="Copy"
         />
-      </div>
+      </div> */}
           <hr className="mt-3" />
           <div>
             <label className="block text-white font-semibold text-2xl mb-5 mt-2">
@@ -81,6 +88,15 @@ function RegistrationForm() {
               type="text"
               required
               placeholder="Full Name"
+            />
+            <label className="block text-white font-semibold text-2xl mb-5 mt-2">
+Location            </label>
+            <input
+              onChange={(e) => setLoc(e.target.value)}
+              className="border w-full text-base px-2 py-1 focus:outline-none focus:ring-0 focus:border-gray-600 rounded-md"
+              type="text"
+              required
+              placeholder="Location"
             />
             <div>
               <label className="block text-white font-semibold text-2xl mb-5 mt-2">
@@ -107,7 +123,29 @@ function RegistrationForm() {
                 onChange={(e) => setDail(e.target.value)}
               />
             </div>
-            <div>
+              <label className="block text-white font-semibold text-2xl mb-5 mt-2">
+                Age
+              </label>
+              <input
+                type="text"
+                id="age"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="Age"
+                required
+                onChange={(e) => setAge(e.target.value)}
+              />
+               <label className="block text-white font-semibold text-2xl mb-5 mt-2">
+                Gender
+              </label>
+              <input
+                type="text"
+                id="gender"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="Gender"
+                required
+                onChange={(e) => setGen(e.target.value)}
+              />
+            {/* <div>
               <label className="block text-white font-semibold text-2xl mb-5 mt-2">
                 Upload Payment Receipt
               </label>
@@ -118,12 +156,11 @@ function RegistrationForm() {
                 required
                 onChange={(e) => handleUpload(e)}
               />
-            </div>
+            </div> */}
           </div>
           <button
             onClick={handleSubmit}
             type="button"
-            disabled={isButtonDisabled}
             className="text-white mt-6 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-10 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Submit
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
@@ -38,38 +39,40 @@ function Home() {
     //     </div>
     //   </div>
     // </div>
-    <div className="bg-[url('brown.jpg')] bg-contain">
-  <div className="bg-contain bg-right mix-blend bg-no-repeat h-screen w-screen bg-[url('bgimagess.png')]">
-    <nav className="flex justify-center items-center">
-      <img src="actions.png" alt="logo" className="p-5 h-24 w-32" />
-    </nav>
-    <div className="text-center p-20">
-      <p className="text-white font-medium tracking-wider text-2xl ml-2 sm:text-xl">
-        Welcome to
-      </p>
-      <h2 className="text-3xl font-bold mt-5
-      
-       tracking-wide text-white sm:text-6xl">
-        ACTION MASTERCLASS
-        <br />
-        ACADEMY
-      </h2>
-    </div>
-    <div className="-mt-8 text-center">
-      <p className="text-lg font-bold mt-2 leading-8 text-white p-4 sm:text-md">
-        Want to get the best and master techniques to shoot your very own movie?
-      </p>
-    </div>
-    <div className="mt-8  flex items-center justify-center gap-x-6 lg:justify-start">
-      <button
-        type="button"
-        className="inline-flex items-center rounded-md sm:mx-auto hover:bg-amber-400 bg-white px-3 py-2 font-bold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300"
-      >
-        <a href="https://paystack.com/pay/zeyhc54q2m">Register Now</a>
-      </button>
+      <div className="bg-[url('brown.jpg')] bg-contain">
+    <div className="bg-contain bg-right mix-blend bg-no-repeat h-screen w-screen bg-[url('bgimagess.png')]">
+      <nav className="flex justify-center items-center">
+        <img src="actions.png" alt="logo" className="p-5 h-24 w-32" />
+      </nav>
+      <div className="text-center p-20">
+        <p className="text-white font-medium tracking-wider text-2xl ml-2 sm:text-xl">
+          Welcome to
+        </p>
+        <h2 className="text-3xl font-bold mt-5
+        
+        tracking-wide text-white sm:text-6xl">
+          ACTION MASTERCLASS
+          <br />
+          ACADEMY
+        </h2>
+      </div>
+      <div className="-mt-8 text-center">
+        <p className="text-lg font-bold mt-2 leading-8 text-white p-4 sm:text-md">
+          Want to get the best and master techniques to shoot your very own movie?
+        </p>
+      </div>
+      <div className="mt-8  flex items-center justify-center gap-x-6 lg:justify-start">
+        <button
+          type="button"
+          className="inline-flex items-center rounded-md sm:mx-auto hover:bg-amber-400 bg-white px-3 py-2 font-bold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300"
+        >
+          {/* <a href="https://paystack.com/pay/zeyhc54q2m">Register Now</a> */}
+          <Link to="/registration">Register Now</Link>
+        </button>
+        
+      </div>
     </div>
   </div>
-</div>
   );
 }
 
